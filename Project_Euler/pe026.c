65e572b6f1600344c12296dace74e6606b0e95f4
Daniel Seita <takeshidanny@gmail.com>
2016-12-30 14:26:21 -0500

    PE 026

---
+++ Project_Euler/pe026.c
@@ -0,0 +1,15 @@
+/**
+ * PE 026: Reciprocal Cycles. Looking back at my old notes, I actually did this
+ * one by hand. Basically start at 1/999 and work down, and in this case it was
+ * easy because the answer has a sequence which is of period d-1, and we know we
+ * can stop there. Interesting, if we have 1/d, why is the length of the cycle
+ * required to be <= d-1? AH, I see on the discussion forum. Nice.
+ */
+
+#include <stdio.h>
+
+
+int main(void) 
+{
+    printf("Sorry, I did this one using paper and pencil (and wolfram alpha).\n");
+}
