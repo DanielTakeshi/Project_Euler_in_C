4d7fc3d4608edd85dbee501c2178fa918b0ee867
Daniel Seita <takeshidanny@gmail.com>
2016-12-31 15:27:51 -0500

    PE 079

---
+++ Project_Euler/pe079.c
@@ -0,0 +1,15 @@
+/**
+ * PE 079: Passcode Derivation. I did this by hand since the problem is
+ * fortunately easy enough (only 50 numbers). After scanning the file I observed
+ * some patterns (e.g. 7 comes early, 0 comes last) and the key is to check
+ * pairs of numbers to see which comes before/after. Fortunately only 8 digits
+ * are needed (the problem could have easily been much harder) so no code is
+ * necessary.
+ */
+
+#include <stdio.h>
+
+int main(void) 
+{
+    printf("pencil and paper\n");
+}
